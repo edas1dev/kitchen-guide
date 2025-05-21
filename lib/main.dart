966b29d7a6a77893f98ee26f5b723a5de3171699
edas1dev <erickdavi200308@gmail.com>
2025-05-21 08:39:27 -0300

    Splash do app concluído (sujeito à alterações).

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:kitchen_guide/home_page.dart';
-import 'package:kitchen_guide/splash_page.dart';
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     home: HomePage(),
-    /*initialRoute: 'splash_page',
-    routes: {
-      'splash_page': (context) => splashPage(),
-      'home_page': (context) => HomePage(),
-      },*/
     )
   );
 }
