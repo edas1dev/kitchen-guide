b4638657b7ef6a4c9c313338972950c7fe96d416
edas1dev <erickdavi200308@gmail.com>
2025-05-31 11:55:55 -0300

    Container 'User Info' criada.

---
+++ lib/profile/profile_containers/container_user_info.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+class ContainerUserInfo extends StatefulWidget {
+  const ContainerUserInfo({super.key});
+
+  @override
+  State<ContainerUserInfo> createState() => _ContainerUserInfoState();
+}
+
+class _ContainerUserInfoState extends State<ContainerUserInfo> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 24),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(13),
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Color(0xFFD9D9D9).withOpacity(0.5),
+              spreadRadius: 4,
+              blurRadius: 4,
+              offset: Offset(0, 4),
+            )
+          ]
+      ),
+      width: 400,
+      height: 130,
+      child: Row(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            spacing: 2,
+            children: [
+              Text(
+                'User Name',
+                style: TextStyle(
+                  color: Color(0xFF1C1C1C),
+                  fontSize: 20,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              Text(
+                'exemple@gmail.com',
+                style: TextStyle(
+                  color: Color(0xFF1C1C1C),
+                  fontSize: 16,
+                  fontWeight: FontWeight.w300,
+                ),
+              ),
+            ],
+          ),
+          Spacer(),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(100),
+                child: Image.network(
+                  //'https://i.pinimg.com/236x/05/ab/3f/05ab3f93b3c22b80d9128532fd0fe2a6.jpg',
+                  'https://i.pinimg.com/736x/fd/fc/ef/fdfcefc24e58a4e3ed4dd6099d530353.jpg',
+                  height: 95, width: 95,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
