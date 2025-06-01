0eb28462eccf360b9342b4497c0017c806e40bb6
edas1dev <erickdavi200308@gmail.com>
2025-06-01 20:21:22 -0300

    Container 'Gerenciar conta' adicionada.

---
+++ lib/profile/profile_containers/container_manage_account.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class ContainerManageAccount extends StatefulWidget {
+  const ContainerManageAccount({super.key});
+
+  @override
+  State<ContainerManageAccount> createState() => _ContainerManageAccountState();
+}
+
+class _ContainerManageAccountState extends State<ContainerManageAccount> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 15),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(13),
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Color(0xFFD9D9D9).withOpacity(0.5),
+              spreadRadius: 4,
+              blurRadius: 4,
+              offset: Offset(0, 4),
+            )
+          ]
+      ),
+      width: double.infinity,
+      height: 80,
+      child: Row(
+        spacing: 10,
+        children: [
+          Icon(Icons.manage_accounts, size: 40),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Gerenciar conta', style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500)),
+              Text('Edite com detalhes a sua conta.'),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
