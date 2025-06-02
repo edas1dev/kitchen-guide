8d2c79c97bf06ee39f8aa41d8ab5bbf7b0f7d66e
edas1dev <erickdavi200308@gmail.com>
2025-06-02 09:02:15 -0300

    Upgrade no arquivo 'Onboarding1' adiconada.

--- lib/onboardings/onboarding1.dart
+++ lib/onboardings/onboarding1.dart
@@ -14,65 +14,47 @@ class _Onboarding1State extends State<Onboarding1> {
       backgroundColor: Colors.white,
       body: Column(
         children: [
-          Image.network('https://img.freepik.com/fotos-premium/dieta-cetonica-de-comida-saudavel-salada-fresca-com-file-de-frango-e-legumes-em-imagem-vertical-preta_1040174-430.jpg'),
+          Image.asset('assets/images/foodOn1.jpg'),
           Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
+            padding: const EdgeInsets.symmetric(horizontal: 25),
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Padding(
-                  padding: const EdgeInsets.only(top: 20),
-                  child: Column(
-                    children: [
-                      Text(
-                        'Descubra Receitas Personalizadas',
-                        style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
-                      ),
-                    ],
-                  ),
+                SizedBox(height: 30),
+                Text(
+                  'Descubra Receitas Personalizadas',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 20, bottom: 70),
-                  child: Column(
-                    spacing: 5,
-                    children: [
-                      Text(
-                          'Conte-nos suas preferências alimentares e nós',
-                          style: TextStyle(fontSize: 14, fontWeight: FontWeight.w400),
-                      ),
-                      Text(
-                          ' criaremos receitas deliciosas para você!',
-                          style: TextStyle(fontSize: 14, fontWeight: FontWeight.w400),
-                      ),
-                    ],
-                  ),
+                SizedBox(height: 20),
+                Column(
+                  children: [
+                    Text(
+                      'Conte-nos suas preferências alimentares e nós',
+                      style: TextStyle(fontSize: 14, fontWeight: FontWeight.w400),
+                    ),
+                    Text(
+                      ' criaremos receitas deliciosas para você!',
+                      style: TextStyle(fontSize: 14, fontWeight: FontWeight.w400),
+                    ),
+                  ],
                 ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      ElevatedButton(onPressed: () {},
-                        style: ElevatedButton.styleFrom(
-                          foregroundColor: Color(0xFFEF233C),
-                        ),
-                        child: Text(
-                          'Pular',
-                          style: TextStyle(fontWeight: FontWeight.w900),
-                        ),
+                SizedBox(height: 110),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    ElevatedButton(onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        foregroundColor: Color(0xFFEF233C),
                       ),
-                      ElevatedButton(onPressed: () {},
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Color(0xFFEF233C),
-                        ),
-                        child: Text(
-                          'Próximo',
-                          style: TextStyle(color: Colors.white, fontWeight: FontWeight.w900),
-                        ),
+                      child: Text('Pular', style: TextStyle(fontWeight: FontWeight.w900)),
+                    ),
+                    ElevatedButton(onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color(0xFFEF233C),
                       ),
-                    ],
-                  ),
-                )
+                      child: Text('Próximo', style: TextStyle(color: Colors.white, fontWeight: FontWeight.w900)),
+                    ),
+                  ],
+                ),
               ],
             ),
           ),
