796941085e557e0e38d32a0e56c1a0d895ea64e8
GleyceBarb <bgleyceeeeeee@gmail.com>
2025-07-29 08:37:07 -0300

    Domain categoria adicionado

---
+++ lib/domain/category.dart
@@ -0,0 +1,11 @@
+class Category {
+  late String titulo;
+  late String subtitulo;
+  late String url;
+
+  Category({
+    required this.titulo,
+    required this.subtitulo,
+    required this.url,
+  });
+}
