002e8eee96bfb97d53ea9a3aadcd7ed93a2f464f
edas1dev <erickdavi200308@gmail.com>
2025-08-07 16:08:32 -0300

    Funções de update para nome, email,senha e deletar conta adiconadas.

--- lib/db/profile_dao.dart
+++ lib/db/profile_dao.dart
@@ -1,17 +1,12 @@
 import 'package:kitchen_guide/domain/profile.dart';
 import 'package:sqflite/sqflite.dart';
-
 import 'db_helper.dart';
 
 class ProfileDao {
-  // Método para inserir um novo perfil
-  // Usamos 'replace' no conflictAlgorithm para que, se você tentar inserir
-  // um perfil com um email que já existe (e se o email for UNIQUE na tabela),
-  // ele substitua o antigo.
   Future<void> insertProfile(Profile profile) async {
     Database db = await DBHelper.initDB();
     await db.insert(
-      'Profile', // Nome da tabela
+      'Profile',
       profile.toJson(),
       conflictAlgorithm: ConflictAlgorithm.replace,
     );
@@ -26,13 +21,49 @@ class ProfileDao {
 
   Future<Profile?> getFirstUser() async {
     Database db = await DBHelper.initDB();
-
     String sql = 'SELECT * FROM Profile;';
     List<Map<String, dynamic>> maps = await db.rawQuery(sql);
-
     if (maps.isNotEmpty)
       return Profile.fromJson(maps.first);
-
     return null;
+  }
+
+  Future<int> updateUserName(String oldEmail, String newName) async {
+    Database db = await DBHelper.initDB();
+    return await db.update(
+      'Profile',
+      {'nome': newName},
+      where: 'email = ?',
+      whereArgs: [oldEmail],
+    );
+  }
+
+  Future<int> updateUserEmail(String oldEmail, String newEmail) async {
+    Database db = await DBHelper.initDB();
+    return await db.update(
+      'Profile',
+      {'email': newEmail},
+      where: 'email = ?',
+      whereArgs: [oldEmail],
+    );
+  }
+
+  Future<int> updateUserPassword(String email, String newPassword) async {
+    Database db = await DBHelper.initDB();
+    return await db.update(
+      'Profile',
+      {'password': newPassword},
+      where: 'email = ?',
+      whereArgs: [email],
+    );
+  }
+
+  Future<int> deleteAccount(String email) async {
+    Database db = await DBHelper.initDB();
+    return await db.delete(
+      'Profile',
+      where: 'email = ?',
+      whereArgs: [email],
+    );
   }
 }
