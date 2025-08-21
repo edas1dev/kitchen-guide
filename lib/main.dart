0ad58c051ab3f12239a10299c9c814f4e2a3d383
edas1dev <erickdavi200308@gmail.com>
2025-08-21 20:12:08 -0300

    Validação de estados para melhor fluxo do app.

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,31 @@
 import 'package:flutter/material.dart';
-import 'package:kitchen_guide/db/profile_dao.dart';
 import 'package:kitchen_guide/pages/display_page.dart';
 import 'package:kitchen_guide/pages/onboardings/onboarding_builder.dart';
 import 'package:kitchen_guide/pages/login/singUpPage.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences prefs = await SharedPreferences.getInstance();
 
-  Widget page = DisplayPage();
-  if(!await ProfileDao().isUserLogged()) {
-      page = OnboardingBuilder(destinyPage: SingUpPage(destinyPage: page),
+  bool hasSeenOnboarding = prefs.getBool('hasSeenOnboarding') ?? false;
+  bool isUserLogged = prefs.getBool('isUserLogged') ?? false;
+
+  Widget page;
+  if (!hasSeenOnboarding) {
+    page = OnboardingBuilder(
+      destinyPage: SingUpPage(destinyPage: DisplayPage()),
     );
+  } else if (!isUserLogged) {
+    page = SingUpPage(destinyPage: DisplayPage());
+  } else {
+    page = DisplayPage();
   }
 
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: page
-    )
+      home: page,
+    ),
   );
 }
