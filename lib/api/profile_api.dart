094deea377204529e632f782835f4607bc13e4b6
edas1dev <erickdavi200308@gmail.com>
2025-10-20 10:19:15 -0300

    Api de imagens de perfil aleatórias adicionada.

--- lib/api/profile_api.dart
+++ lib/api/profile_api.dart
@@ -1,20 +1,29 @@
 import 'package:dio/dio.dart';
-import 'package:kitchen_guide/domain/profile.dart';
+import 'dart:math';
 
 class ProfileApi {
   final dio = Dio();
-  final String baseUrl = 'https://my-json-server.typicode.com/gleycebarb/fake-api';
+  final String fakeApiBaseUrl = 'https://my-json-server.typicode.com/gleycebarb/fake-api';
+  final String avatarGeneratorBaseUrl = 'https://testingbot.com/free-online-tools/random-avatar';
+
+  String getAvatarUrlWithRandomSize(int maxSize) {
+    final random = Random();
+    final randomSize = random.nextInt(maxSize) + 1;
+    return '$avatarGeneratorBaseUrl/$randomSize';
+  }
 
   Future<Map<String, dynamic>> fetchProfileByEmail(String email) async {
     try {
-      final response = await dio.get('$baseUrl/Profile');
+      final response = await dio.get('$fakeApiBaseUrl/Profile');
+      final String randomImageUrl = getAvatarUrlWithRandomSize(1000);
+
       if (response.statusCode == 200 && response.data != null) {
         final List profiles = response.data;
-        final profileData = profiles.firstWhere(
-          (p) => p['email'] == email,
-          orElse: () => null
-        );
+        final profileData = profiles.firstWhere((p) => p['email'] == email, orElse: () => null);
         if (profileData != null) {
+          if (profileData['urlImage'] == null || profileData['urlImage'].isEmpty) {
+            profileData['urlImage'] = randomImageUrl;
+          }
           return profileData;
         } else {
           throw Exception('Perfil não encontrado para o email informado.');
--- lib/pages/profile/container_user_info.dart
+++ lib/pages/profile/container_user_info.dart
@@ -4,27 +4,29 @@ import 'package:kitchen_guide/domain/profile.dart';
 class ContainerUserInfo extends StatelessWidget {
   final Profile? userProfile;
   const ContainerUserInfo({super.key, this.userProfile});
+  static const String defaultUrl = 'assets/images/default_pfp.jpg';
 
   @override
   Widget build(BuildContext context) {
     final String userName = userProfile?.nome ?? '...';
     final String userEmail = userProfile?.email ?? '...';
-    final String userProfileImage = userProfile?.urlImage ?? 'assets/images/default_pfp.jpg';
-    final bool isNetworkImage = userProfileImage.startsWith('http') || userProfileImage.startsWith('https');
+    final String userProfileImage = userProfile?.urlImage ?? defaultUrl;
+    final String finalImage = (userProfileImage.isEmpty || userProfileImage == defaultUrl) ? defaultUrl : userProfileImage;
+    final bool isNetworkImage = finalImage.startsWith('http') || finalImage.startsWith('https');
 
     return Container(
       padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(13),
-        color: Colors.white,
-        boxShadow: [
-          BoxShadow(
-            color: Color(0xFFD9D9D9).withOpacity(0.5),
-            spreadRadius: 4,
-            blurRadius: 4,
-            offset: Offset(0, 4),
-          )
-        ]
+          borderRadius: BorderRadius.circular(13),
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Color(0xFFD9D9D9).withOpacity(0.5),
+              spreadRadius: 4,
+              blurRadius: 4,
+              offset: Offset(0, 4),
+            )
+          ]
       ),
       width: double.infinity,
       child: Row(
@@ -36,18 +38,18 @@ class ContainerUserInfo extends StatelessWidget {
               children: [
                 Text(
                   userName, style: TextStyle(
-                    color: Color(0xFF1C1C1C),
-                    fontSize: 20,
-                    fontWeight: FontWeight.w500,
-                  ),
+                  color: Color(0xFF1C1C1C),
+                  fontSize: 20,
+                  fontWeight: FontWeight.w500,
+                ),
                 ),
                 const SizedBox(height: 2),
                 Text(
                   userEmail, style: TextStyle(
-                    color: Color(0xFF1C1C1C),
-                    fontSize: 16,
-                    fontWeight: FontWeight.w300,
-                  ),
+                  color: Color(0xFF1C1C1C),
+                  fontSize: 16,
+                  fontWeight: FontWeight.w300,
+                ),
                 ),
               ],
             ),
@@ -56,21 +58,11 @@ class ContainerUserInfo extends StatelessWidget {
           ClipRRect(
             borderRadius: BorderRadius.circular(60),
             child: isNetworkImage ? Image.network(
-              userProfileImage, width: 95, height: 95, fit: BoxFit.cover,
+              finalImage, width: 95, height: 95, fit: BoxFit.cover,
               errorBuilder: (context, error, stackTrace) {
-                return Image.asset(
-                  'assets/images/default_pfp.jpg',
-                  width: 95,
-                  height: 95,
-                  fit: BoxFit.cover,
-                );
+                return Image.asset(defaultUrl, width: 95, height: 95, fit: BoxFit.cover);
               },
-            ): Image.asset(
-              userProfileImage,
-              width: 95,
-              height: 95,
-              fit: BoxFit.cover,
-            ),
+            ) : Image.asset(finalImage, width: 95, height: 95, fit: BoxFit.cover),
           ),
         ],
       ),
